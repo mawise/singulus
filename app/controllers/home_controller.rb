b5ff0934b740145b9c4775e145859ac7166f42da
Tony Burns <tony@tonyburns.net>
2020-07-25 15:48:37 -0400

    feat: create homepage

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# The homepage controller.
+class HomeController < ApplicationController
+  def index; end
+end
---
+++ app/helpers/home_helper.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# Helpers for the homepage.
+module HomeHelper
+end
