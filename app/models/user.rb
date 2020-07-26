332d24eed1cba4bffbf88b241ffc3ef755aa4485
Tony Burns <tony@tonyburns.net>
2020-07-25 20:36:10 -0400

    feat: add OAuth relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,14 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :lockable,
          :rememberable, :trackable, :validatable
+
+  has_many :access_grants, # rubocop:disable Rails/InverseOf
+           class_name: 'Doorkeeper::AccessGrant',
+           foreign_key: :resource_owner_id,
+           dependent: :delete_all
+
+  has_many :access_tokens, # rubocop:disable Rails/InverseOf
+           class_name: 'Doorkeeper::AccessToken',
+           foreign_key: :resource_owner_id,
+           dependent: :delete_all
 end
