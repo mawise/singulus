3a8eadc718444d2970a016207e2cb0abe51bdd35
Tony Burns <tony@tonyburns.net>
2020-08-01 16:24:25 -0400

    ci: service containers for minio and elasticsearch

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -2,10 +2,21 @@
 
 require 'shrine/storage/s3'
 
-s3_options = {
-  bucket: ENV.fetch('ASSETS_BUCKET'),
-  public: true
-}
+s3_options = if Rails.env.test?
+               {
+                 access_key_id: ENV.fetch('MINIO_ACCESS_KEY'),
+                 secret_access_key: ENV.fetch('MINIO_SECRET_KEY'),
+                 endpoint: 'http://localhost:9000',
+                 bucket: 'singulus-test',
+                 region: 'us-east-1',
+                 force_path_style: true
+               }
+             else
+               {
+                 bucket: ENV.fetch('ASSETS_BUCKET'),
+                 public: true
+               }
+             end
 
 Shrine.logger = Rails.logger
 Shrine.logger.level = Logger::WARN if Rails.env.test?
