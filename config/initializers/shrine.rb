131023c44569d153867c6b3fe4bd9f1786a0ef16
Tony Burns <tony@tonyburns.net>
2020-07-31 22:25:34 -0400

    chore: log and instrument for shrine

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -7,10 +7,14 @@ s3_options = {
   public: true
 }
 
+Shrine.logger = Rails.logger
+Shrine.logger.level = Logger::WARN if Rails.env.test?
+
 Shrine.storages = {
   cache: Shrine::Storage::S3.new(prefix: 'cache', **s3_options),
   store: Shrine::Storage::S3.new(**s3_options)
 }
 
+Shrine.plugin :instrumentation, notifications: ActiveSupport::Notifications
 Shrine.plugin :activerecord
 Shrine.plugin :url_options, store: { host: ENV.fetch('ASSETS_URL') }
