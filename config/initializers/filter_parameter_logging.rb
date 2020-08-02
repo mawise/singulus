c7d0a6e41ca9af6d139b53278cd97340e2c7c518
Tony Burns <tony@tonyburns.net>
2020-08-02 00:25:07 -0400

    feat: additional fields for log filtering

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
-# Be sure to restart your server when you modify this file.
-
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password]
+Rails.application.config.filter_parameters += %i[
+  access_token
+  client_secret
+  code
+  password
+  password_confirmation
+  refresh_token
+]
