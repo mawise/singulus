a4570435f4952e4e8eb06cabfbbef9da54adae0b
Tony Burns <tony@tonyburns.net>
2020-08-02 00:29:44 -0400

    refactor: clean up initializers

--- config/initializers/cookies_serializer.rb
+++ config/initializers/cookies_serializer.rb
@@ -1,7 +1,4 @@
 # frozen_string_literal: true
 
-# Be sure to restart your server when you modify this file.
-
-# Specify a serializer for the signed and encrypted cookie jars.
-# Valid options are :json, :marshal, and :hybrid.
+# Serializer for the signed and encrypted cookie jars.
 Rails.application.config.action_dispatch.cookies_serializer = :json
