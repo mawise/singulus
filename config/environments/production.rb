9e221907963a6de9141d35f756c7636ce8152975
Tony Burns <tony@tonyburns.net>
2020-08-02 00:47:41 -0400

    refactor: clean up initializers

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,6 +5,8 @@ Rails.application.configure do
   config.cache_classes = true
   config.consider_all_requests_local = false
   config.eager_load = true
+
+  # Public file server configuration
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Logging configuration
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,52 +1,38 @@
 # frozen_string_literal: true
 
-# The test environment is used exclusively to run your application's
-# test suite. You never need to work with it otherwise. Remember that
-# your test database is "scratch space" for the test suite and is wiped
-# and recreated between test runs. Don't rely on the data there!
-
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
-
+  # General Rails configuration
   config.cache_classes = false
-  config.action_view.cache_template_loading = true
-
-  # Do not eager load code on boot. This avoids loading your whole application
-  # just for the purpose of running a single test. If you are using a tool that
-  # preloads Rails for running tests, you may have to set it to true.
+  config.cache_store = :null_store
+  config.consider_all_requests_local = true
   config.eager_load = false
 
-  # Configure public file server for tests with Cache-Control for performance.
+  # Public file server configuration
   config.public_file_server.enabled = true
   config.public_file_server.headers = {
     'Cache-Control' => "public, max-age=#{1.hour.to_i}"
   }
 
-  # Show full error reports and disable caching.
-  config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
-  config.cache_store = :null_store
+  # Assets configuration
+  config.assets.url = 'http://localhost:9000/singulus-test/'
 
-  # Raise exceptions instead of rendering exception templates.
+  # ActionController configuration
+  config.action_controller.allow_forgery_protection = false
+  config.action_controller.perform_caching = false
+
+  # ActionDispatch configuration
   config.action_dispatch.show_exceptions = false
 
-  # Disable request forgery protection in test environment.
-  config.action_controller.allow_forgery_protection = false
-
+  # ActionMailer configuration
+  config.action_mailer.delivery_method = :test
   config.action_mailer.perform_caching = false
 
-  # Tell Action Mailer not to deliver emails to the real world.
-  # The :test delivery method accumulates sent emails in the
-  # ActionMailer::Base.deliveries array.
-  config.action_mailer.delivery_method = :test
+  # ActionView configuration
+  config.action_view.cache_template_loading = true  
 
-  # Print deprecation notices to the stderr.
+  # ActiveSupport configuration
   config.active_support.deprecation = :stderr
 
-  # Raises error for missing translations.
-  # config.action_view.raise_on_missing_translations = true
-
+  # rack-attack configuration
   Rack::Attack.enabled = false
-
-  config.assets.url = 'http://localhost:9000/singulus-test/'
 end
