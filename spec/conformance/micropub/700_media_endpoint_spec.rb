a59c8c30cd89a2e3eb68f83f3c3d6583412d57d4
Tony Burns <tony@tonyburns.net>
2020-08-01 21:48:34 -0400

    refactor: rename assets to photos

--- spec/conformance/micropub/700_media_endpoint_spec.rb
+++ spec/conformance/micropub/700_media_endpoint_spec.rb
@@ -20,22 +20,22 @@ RSpec.describe 'Micropub Server Implementation Report - Media Endpoint', type: :
   describe '700 - Upload a jpg to the Media Endpoint' do
     let(:photo) { fixture_file_upload(Rails.root.join('spec/fixtures/photos/sunset.jpg'), 'image/jpeg') }
     let(:params) { { file: photo } }
-    let(:new_asset) { Photo.where('file_data @> ?', { metadata: { filename: 'sunset.jpg' } }.to_json).first }
+    let(:new_photo) { Photo.where('file_data @> ?', { metadata: { filename: 'sunset.jpg' } }.to_json).first }
 
     it 'returns HTTP created' do
       expect(response).to have_http_status(:created)
     end
 
-    it 'returns a Location header with the URL of the new asset' do
-      expect(response.headers['Location']).to eq(new_asset.file_url)
+    it 'returns a Location header with the URL of the new photo' do
+      expect(response.headers['Location']).to eq(new_photo.file_url)
     end
 
-    it 'creates an orphan asset' do
-      expect(new_asset).not_to be_nil
+    it 'creates an orphan photo' do
+      expect(new_photo).not_to be_nil
     end
 
-    it 'sets the mime type for the asset' do
-      expect(new_asset.mime_type).to eq('image/jpeg')
+    it 'sets the mime type for the photo' do
+      expect(new_photo.mime_type).to eq('image/jpeg')
     end
   end
 
@@ -43,22 +43,22 @@ RSpec.describe 'Micropub Server Implementation Report - Media Endpoint', type: :
   describe '701 - Upload a png to the Media Endpoint' do
     let(:photo) { fixture_file_upload(Rails.root.join('spec/fixtures/photos/micropub-rocks.png'), 'image/png') }
     let(:params) { { file: photo } }
-    let(:new_asset) { Photo.where('file_data @> ?', { metadata: { filename: 'micropub-rocks.png' } }.to_json).first }
+    let(:new_photo) { Photo.where('file_data @> ?', { metadata: { filename: 'micropub-rocks.png' } }.to_json).first }
 
     it 'returns HTTP created' do
       expect(response).to have_http_status(:created)
     end
 
-    it 'returns a Location header with the URL of the new asset' do
-      expect(response.headers['Location']).to eq(new_asset.file_url)
+    it 'returns a Location header with the URL of the new photo' do
+      expect(response.headers['Location']).to eq(new_photo.file_url)
     end
 
-    it 'creates an orphan asset' do
-      expect(new_asset).not_to be_nil
+    it 'creates an orphan photo' do
+      expect(new_photo).not_to be_nil
     end
 
-    it 'sets the mime type for the asset' do
-      expect(new_asset.mime_type).to eq('image/png')
+    it 'sets the mime type for the photo' do
+      expect(new_photo.mime_type).to eq('image/png')
     end
   end
 
@@ -66,22 +66,22 @@ RSpec.describe 'Micropub Server Implementation Report - Media Endpoint', type: :
   describe '702 - Upload a gif to the Media Endpoint' do
     let(:photo) { fixture_file_upload(Rails.root.join('spec/fixtures/photos/w3c-socialwg.gif'), 'image/gif') }
     let(:params) { { file: photo } }
-    let(:new_asset) { Photo.where('file_data @> ?', { metadata: { filename: 'w3c-socialwg.gif' } }.to_json).first }
+    let(:new_photo) { Photo.where('file_data @> ?', { metadata: { filename: 'w3c-socialwg.gif' } }.to_json).first }
 
     it 'returns HTTP created' do
       expect(response).to have_http_status(:created)
     end
 
-    it 'returns a Location header with the URL of the new asset' do
-      expect(response.headers['Location']).to eq(new_asset.file_url)
+    it 'returns a Location header with the URL of the new photo' do
+      expect(response.headers['Location']).to eq(new_photo.file_url)
     end
 
-    it 'creates an orphan asset' do
-      expect(new_asset).not_to be_nil
+    it 'creates an orphan photo' do
+      expect(new_photo).not_to be_nil
     end
 
-    it 'sets the mime type for the asset' do
-      expect(new_asset.mime_type).to eq('image/gif')
+    it 'sets the mime type for the photo' do
+      expect(new_photo.mime_type).to eq('image/gif')
     end
   end
 end
