7e0cd656a5bd50f9a7789c7c487367284da7c5ab
Tony Burns <tony@tonyburns.net>
2020-08-05 22:55:03 -0400

    feat: generate dashboard controllers for webmentions

---
+++ app/controllers/dashboard/webmentions_controller.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Dashboard
+  # Manages webmentions.
+  class WebmentionsController < ApplicationController
+  end
+end
---
+++ app/helpers/dashboard/webmentions_helper.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Dashboard
+  # Helpers for dashboard webmention views.
+  module WebmentionsHelper
+  end
+end
