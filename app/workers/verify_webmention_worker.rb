6684e0e7b2579052ebb3b9356f73bb57ec600c9c
Tony Burns <tony@tonyburns.net>
2020-08-05 22:55:24 -0400

    feat: spike on webmention verification

--- app/workers/verify_webmention_worker.rb
+++ app/workers/verify_webmention_worker.rb
@@ -6,5 +6,17 @@ class VerifyWebmentionWorker < ApplicationWorker
 
   def perform(id)
     @webmention = Webmention.find(id)
+
+    resp = Net::HTTP.get(@webmention.source_uri)
+    props = Microformats.parse(resp).to_h
+  end
+
+  private
+
+  def retrieve_source
+    req = Typhoeus::Request.new(@webmention.source_url, followlocation: true)
+    req.on_complete do |res|
+    end
+    req.run
   end
 end
