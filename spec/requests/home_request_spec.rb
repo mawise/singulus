af7563b58422d9814f6e3d1fd99b7df25bfe4222
Tony Burns <tony@tonyburns.net>
2020-08-16 18:18:13 -0400

    test: homepage redirect when not logged in

--- spec/requests/home_request_spec.rb
+++ spec/requests/home_request_spec.rb
@@ -4,20 +4,22 @@ require 'rails_helper'
 
 RSpec.describe '/', type: :request do
   describe 'GET /' do
-    it 'returns HTTP success' do
-      get '/'
-      expect(response).to have_http_status(:success)
+    context 'when logged in' do
+      let(:user) { FactoryBot.create(:user) }
+
+      before { sign_in(user) }
+
+      it 'redirects to the dashboard' do
+        get '/'
+        expect(response).to redirect_to(dashboard_root_path)
+      end
     end
-  end
 
-  context 'when already logged in' do
-    let(:user) { FactoryBot.create(:user) }
-
-    before { sign_in(user) }
-
-    it 'redirects to the dashboard' do
-      get '/'
-      expect(response).to redirect_to(dashboard_root_path)
+    context 'when not logged in' do
+      it 'returns redirects to the login page' do
+        get '/'
+        expect(response).to redirect_to(new_user_session_path)
+      end
     end
   end
 end
