c703798ce2cc2f9ecc45d140d2d1b98504b5365c
Tony Burns <tony@tonyburns.net>
2020-08-16 19:42:45 -0400

    fix: force URI parse on redirects

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -5,7 +5,7 @@ class LinksController < ApplicationController
   def show
     @link = Link.find_by(name: params[:id])
     if @link.present?
-      redirect_to URI(@link.target_url)
+      redirect_to URI(@link.target_url).to_s
     else
       render :not_found, status: :not_found
     end
