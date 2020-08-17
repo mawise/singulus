95bcb71814fe9ae376f9d3411226382290166cca
Tony Burns <tony@tonyburns.net>
2020-08-16 23:58:13 -0400

    style: prettier

--- app/frontend/controllers/counter_controller.js
+++ app/frontend/controllers/counter_controller.js
@@ -7,24 +7,24 @@
 //   <h1 data-target="hello.output"></h1>
 // </div>
 
-import { Controller } from "stimulus"
+import { Controller } from "stimulus";
 
 export default class extends Controller {
-  static targets = ["input", "count"]
+  static targets = ["input", "count"];
 
   recount() {
     var count = this.inputTarget.value.length;
     this.countTarget.textContent = count;
-    if(count >= parseInt(this.data.get("danger"))) {
-      this.inputTarget.className = 'textarea is-danger';
+    if (count >= parseInt(this.data.get("danger"))) {
+      this.inputTarget.className = "textarea is-danger";
     } else if (count >= parseInt(this.data.get("warn"))) {
-      this.inputTarget.className = 'textarea is-warning';
+      this.inputTarget.className = "textarea is-warning";
     } else {
-      this.inputTarget.className = 'textarea';
+      this.inputTarget.className = "textarea";
     }
   }
 
   connect() {
-    this.recount()
+    this.recount();
   }
 }
