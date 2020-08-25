b652ade48d38597401f900b7548ef75d1abc707f
Tony Burns <tony@tonyburns.net>
2020-08-25 18:44:28 -0400

    fix: plainify meta

--- app/models/concerns/post_metadata.rb
+++ app/models/concerns/post_metadata.rb
@@ -14,6 +14,11 @@ module PostMetadata
     return super if self[:meta_description]
 
     source = summary.presence || content.presence || ''
+    source = plainify(source)
     source.gsub(/(\r)?\n/, ' ').truncate(160)
+  end
+
+  def plainify(source)
+    Nokogiri::HTML(Kramdown::Document.new(String(source), input: 'GFM').to_html).text
   end
 end
