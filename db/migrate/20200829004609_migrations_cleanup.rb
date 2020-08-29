21693ef9d26122beb0baefc8ab722ff4ec1dfc30
Tony Burns <tony@tonyburns.net>
2020-08-29 13:23:46 -0400

    style: empty line after guard clause

--- db/migrate/20200829004609_migrations_cleanup.rb
+++ db/migrate/20200829004609_migrations_cleanup.rb
@@ -6,6 +6,7 @@ class MigrationsCleanup < ActiveRecord::Migration[6.0]
 
   def up
     return unless ActiveRecord::Migrator.current_version < REQUIRED_VERSION
+
     raise StandardError, '`rails db:schema:load` must be run prior to `rails db:migrate`'
   end
 end
